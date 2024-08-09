3e93596d31fda702024c6ffe2645bafd3a57466c
viniciuspadovam <padovam3373@gmail.com>
2024-08-08 22:46:39 -0300

    initial commit

---
+++ src/main/java/com/viniciuspadovam/collabpro/CollabproApplication.java
@@ -0,0 +1,13 @@
+package com.viniciuspadovam.collabpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciuspadovam/collabpro/CollabproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuspadovam.collabpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
